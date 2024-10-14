e501f1c5ae2dd1a6fecae06c25882a3d59bc5f73
aquintero <alejandro_quintero@sil.org>
2024-10-14 13:27:24 -0700

    remote model

---
+++ upload_svd.py
@@ -0,0 +1,33 @@
+# create example dataset
+import argparse
+import os
+from clearml import StorageManager, Dataset, Task
+
+# Add arguments dataset_path, bucket_name, and dataset_name
+parser = argparse.ArgumentParser()
+parser.add_argument("--dataset_path", type=str, default="./models/SVD/stable-video-diffusion-img2vid-xt-1-1")
+parser.add_argument("--bucket_name", type=str, default="sil-mimicmotion")
+parser.add_argument("--dataset_name", type=str, default="SVD-MimicMotion")
+args = parser.parse_args()
+
+# Step 4: Use the parsed arguments in your script
+# Create a dataset with ClearML's Dataset class
+
+dataset = Dataset.create(
+    dataset_project="SVD MimicMotion",
+    dataset_name= f"{args.dataset_name}",
+    output_uri=f"s3://{args.bucket_name}"
+)
+
+# Add the example csv
+dataset.add_files(path=f"{args.dataset_path}")
+
+# Upload dataset to ClearML server (customizable)
+dataset.upload()
+
+# Commit dataset changes
+dataset.finalize()
+
+
+
+
